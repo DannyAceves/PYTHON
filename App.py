9124c583bc5543c084de42ea2b8aa9fafa181421
Daniel Aceves Espinoza <71477080+DannyAceves@users.noreply.github.com>
2024-06-04 14:24:03 -0600

    PRIMER COMMIT DEL REPO

---
+++ App.py
@@ -0,0 +1,22 @@
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route('/')
+def Index():
+    return 'Hello World!'
+
+@app.route('/add conctact')
+def add_contact():
+    return 'Agregar contacto'
+
+@app.route('/edit contact')
+def edit_contact():
+    return 'editar contacto'
+
+@app.route('/delete contact')
+def delete_contact():
+    return 'borrar contacto'
+
+if __name__ == '_main_':
+    app.run(port = 3000, debug = True)
